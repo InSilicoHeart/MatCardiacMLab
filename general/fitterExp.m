7ec9a6320d48c672470e111a051fff68684650bb
Jesús Carro <chuscarro@gmail.com>
2014-07-09 17:41:54 +0200

    File add

---
+++ general/fitterExp.m
@@ -0,0 +1,37 @@
+%% fitterExp - used to calculate the error of fit data to an 
+%            exponential function whith the form: A*exp(-t/B)+C
+%                                
+%
+%     f = fitterExp(x,data)                                    
+%                                                                                                                                                                                                  
+%    Input:                                                                 
+%      x:      Vector with the values of A, B and C.
+%      data:   Matrix whith the value of time and the data to fit in the
+%              first and the second column respectively.               
+%                                                                           
+%    Output:                                                                
+%      f:      Mean square error.                                        
+%
+%-----------------------------------------------------------------------
+% 
+% MatCardiacMLab (v00.00)
+%
+% Matlab toolbox to Simulate Electrophysiologycal Cardiac Models 
+% described in CellML files
+%
+% Jesus Carro Fernandez 
+% jcarro@usj.es  
+%                      
+% School of Engineering
+% San Jorge University 
+% www.usj.es  
+%       
+% Last Modification 2014/07/09
+%
+
+
+function f = fitter_exp(x,data)
+t=data(:,1);
+expon=x(1)*exp(-t/x(2))+x(3);
+
+f=mean((data(:,2)-expon).^2);

60aafb9100577839bb4624f528633934560a6ab1
Jesús Carro <chuscarro@gmail.com>
2014-07-09 09:47:15 +0200

    _

--- tests/mainTest.m
+++ tests/mainTest.m
@@ -22,6 +22,7 @@ disp(' ')
  
 %tests = struct('functionHandle',[],'name',{},'expectedValue',{});
 
+mkdir('testResults')
 tests = [configurationTests()];% runTests()];
 
 results = false(size(tests));

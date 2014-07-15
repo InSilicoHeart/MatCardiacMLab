540812f2beb7687a8a39c89edd59decb662c493e
Jesús Carro <chuscarro@gmail.com>
2014-07-15 11:40:40 +0200

    Removed debug output

--- tests/testBiomarkerAPDRA002.m
+++ tests/testBiomarkerAPDRA002.m
@@ -10,7 +10,6 @@ msg = 'Unknow error';
   t = [0:800]'*0.6;
   APD_r = (APDini-APDend)*(0.5*exp(-t/tauF_r)+0.5*exp(-t/tauS_r))+APDend;
   APD = APD_r + normrnd(0,0.5,size(t));
-  mean(abs(APD_r-APD))
   [tauF_c,tauS_c]=calculateTaus(APD,t');  
   disp(['TauF real = ' num2str(tauF_r) ' --- TauF computed = ' num2str(tauF_c)])
   disp(['TauS real = ' num2str(tauS_r) ' --- TauS computed = ' num2str(tauS_c)])

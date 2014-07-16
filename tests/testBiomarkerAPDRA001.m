56c74b8b234a78c623382db6a498772ce620885e
Jesús Carro <chuscarro@gmail.com>
2014-07-16 13:10:43 +0200

    Modification of test to represent the new algorithm

--- tests/testBiomarkerAPDRA001.m
+++ tests/testBiomarkerAPDRA001.m
@@ -4,11 +4,13 @@ result = false;
 msg = 'Unknow error';
 try
   APDini = 300;
-  APDend = 200;
+  APDend = 250;
   tauF_r = 10;
   tauS_r = 100;
   t = [0:800]'*0.6;
-  APD = (APDini-APDend)*(0.5*exp(-t/tauF_r)+0.5*exp(-t/tauS_r))+APDend;
+  APD = [APDini; APDini*exp(-t(2)/tauF_r)];
+  APD = [APD; (APD(2)-APDend)*exp(-t(3:end)/tauS_r)+APDend];
+
   [tauF_c,tauS_c]=calculateTaus(APD,t);  
   disp(['TauF real = ' num2str(tauF_r) ' --- TauF computed = ' num2str(tauF_c)])
   disp(['TauS real = ' num2str(tauS_r) ' --- TauS computed = ' num2str(tauS_c)])
--- tests/testBiomarkerAPDRA002.m
+++ tests/testBiomarkerAPDRA002.m
@@ -8,7 +8,9 @@ msg = 'Unknow error';
   tauF_r = 10;
   tauS_r = 100;
   t = [0:800]'*0.6;
-  APD_r = (APDini-APDend)*(0.5*exp(-t/tauF_r)+0.5*exp(-t/tauS_r))+APDend;
+  APD_r = [APDini; APDini*exp(-t(2)/tauF_r)];
+  APD_r = [APD_r; (APD_r(2)-APDend)*exp(-t(3:end)/tauS_r)+APDend];
+
   APD = APD_r + normrnd(0,0.5,size(t));
   [tauF_c,tauS_c]=calculateTaus(APD,t);  
   disp(['TauF real = ' num2str(tauF_r) ' --- TauF computed = ' num2str(tauF_c)])
